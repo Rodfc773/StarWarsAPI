83df03baa585cc756d05859ed64f6ab2fa634676
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-10-26 00:18:48 -0300

    Creating teh controller for the Planet Route

---
+++ src/controllers/planetController.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
+import { Planet } from '../models/Planet';
+import { DataBaseError, PlanetMissingDataError } from '../utils/Errors';
+import { ValidationPlanetData } from '../utils/Validations';
+
+const prisma = new PrismaClient();
+
+export class PlanetController {
+  async index(req: Request, res: Response) {
+    try {
+      const planets = await prisma.planet.findMany();
+
+      return res.json(planets);
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json(new DataBaseError('Unable to access the DataBase'));
+    }
+  }
+  async post(req: Request, res: Response) {
+    const { name, terrain, size, population, weather } = req.body;
+
+    try {
+      const planetData = new Planet(name, terrain, size, population, weather);
+      const validatorData = new ValidationPlanetData();
+
+      if (!validatorData.validationData(planetData)) {
+        return res
+          .status(400)
+          .json(
+            new PlanetMissingDataError(
+              "Some data of the new planet is missing or it's null",
+            ),
+          );
+      }
+      const newPlanet = await prisma.planet.create({
+        data: {
+          name: planetData.name,
+          terrain: planetData.terrain,
+          size: planetData.size,
+          population: planetData.population,
+          weather: planetData.weather,
+        },
+      });
+
+      return res.status(201).json(newPlanet);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json(new DataBaseError('Unable to create the new planet'));
+    }
+  }
+}
