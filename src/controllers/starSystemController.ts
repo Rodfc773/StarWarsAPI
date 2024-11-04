21f5b73a8d4b3727d9eab4b07e76052c82c64438
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-04 17:07:44 -0300

    Finishing the Star Systems routes but still needing to do the routes tests

--- src/controllers/starSystemController.ts
+++ src/controllers/starSystemController.ts
@@ -6,13 +6,68 @@ import { Service } from 'src/services/interfaces/Service';
 export class StarSystemController {
   constructor(private service: Service<StarSystem>) {}
 
-  async index(req: Request, res: Response) {
+  async index(req: Request, res: Response): Promise<Response> {
     try {
-      const StarSystems = await this.service.getAll();
+      const StarSystems: StarSystem[] = await this.service.getAll();
 
       return res.status(200).json(StarSystems);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      return res.status(500).json({ message: error.message });
+    }
+  }
+  async show(req: Request, res: Response): Promise<Response> {
+    try {
+      const starSystemName: string = req.params.name;
+
+      const starSystemFound: StarSystem =
+        await this.service.findOne(starSystemName);
+
+      return res.status(200).json(starSystemFound);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+  async post(req: Request, res: Response): Promise<Response> {
+    try {
+      const data = req.body;
+
+      const starSystemData = new StarSystem(data.name, data.description);
+
+      const newStarSystem: StarSystem =
+        await this.service.createOne(starSystemData);
+
+      return res.status(201).json(newStarSystem);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+  async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const { name, description } = req.body;
+      const starSystemName = req.params.name;
+
+      const dataToBeUpdated = new StarSystem(name, description);
+
+      const starSystemUpdated: StarSystem = await this.service.updateOne(
+        starSystemName,
+        dataToBeUpdated,
+      );
+
+      res.status(200).json(starSystemUpdated);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+  async delete(req: Request, res: Response): Promise<Response> {
+    try {
+      const starSystemName = req.params.name;
+
+      const starSystemDeleted: StarSystem =
+        await this.service.deleteOne(starSystemName);
+
+      return res.status(200).json(starSystemDeleted);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
   }
 }
--- src/routes/starSystemRoutes.ts
+++ src/routes/starSystemRoutes.ts
@@ -1,9 +1,20 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import { StarSystemController } from 'src/controllers/starSystemController';
+import { StarSystemRepository } from 'src/repositories/StarSystemRepository';
+import { StarSystemService } from 'src/services/StarsystemService';
+import { StarSystemValidator } from 'src/utils/Validations';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Rota do sistema estelar');
-});
+const repository = new StarSystemRepository();
+const starValidator = new StarSystemValidator();
+const starService = new StarSystemService(repository, starValidator);
+const starController = new StarSystemController(starService);
+
+router.get('/', starController.index.bind(starController));
+router.get('/:name', starController.show.bind(starController));
+router.post('/', starController.post.bind(starController));
+router.put('/:name', starController.update.bind(starController));
+router.delete('/:name', starController.delete.bind(starController));
 
 export default router;
