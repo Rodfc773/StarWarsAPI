329a187acafacf5dedc04ee34be8a82b81370998
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-04 17:06:04 -0300

    Fixing the code's style

--- src/routes/planetsRoutes.ts
+++ src/routes/planetsRoutes.ts
@@ -7,20 +7,15 @@ import { PlanetService } from 'src/services/PlanetService';
 
 const repository = new PlanetRepository();
 const validador = new PlanetDataValidator();
-
 const service = new PlanetService(repository, validador);
-
 const planetController = new PlanetController(service);
 
 const planetRouter = Router();
 
 planetRouter.get('/', planetController.index.bind(planetController));
 planetRouter.get('/:name', planetController.show.bind(planetController));
-
 planetRouter.post('/', planetController.post.bind(planetController));
-
 planetRouter.delete('/:name', planetController.delete.bind(planetController));
-
 planetRouter.patch('/:name', planetController.put.bind(planetController));
 
 export default planetRouter;
