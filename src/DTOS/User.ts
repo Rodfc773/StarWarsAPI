2c33bad56af961acb6dd0c0678627a619dd8d312
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-07 12:07:37 -0300

    Finishing the User DTO class

--- src/DTOS/User.ts
+++ src/DTOS/User.ts
@@ -1,25 +1,49 @@
+import * as bcrypt from 'bcryptjs';
+
 export class UserDTO {
   private _id?: string;
   private _name: string;
   private _email: string;
+  private _password: string;
   private _nickname: string;
   private _createdAt?: Date;
   private _updatedAt?: Date;
 
   constructor(user: {
-    id: string;
+    id?: string;
     name: string;
     email: string;
+    password?: string;
     nickname: string;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
   }) {
     this._id = user.id;
     this._name = user.name;
     this._email = user.email;
     this._nickname = user.nickname;
+    this.setPassword(user.password);
     this._createdAt = user.createdAt;
     this._updatedAt = user.updatedAt;
+  }
+
+  async setPassword(password: string): Promise<void> {
+    const hashedPassword = await bcrypt.hash(password, 11);
+    this._password = hashedPassword;
+  }
+  async verifyPassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this._password);
+  }
+
+  toObject(): object {
+    return {
+      id: this._id,
+      name: this._name,
+      email: this._email,
+      nickname: this._nickname,
+      createdAt: this._createdAt,
+      updateAt: this._updatedAt,
+    };
   }
 
   get name(): Readonly<string> {
