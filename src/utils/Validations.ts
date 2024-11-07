d6c3e91fe7fa761be53cb03eb23e26967a9920aa
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-07 12:06:03 -0300

    Fixing some validations

--- src/utils/Validations.ts
+++ src/utils/Validations.ts
@@ -1,12 +1,12 @@
-import { StarSystem } from '@prisma/client';
-import { Planet } from 'src/DTO's/Planet';
+import { StarSystem } from '../DTOS/Starsystem';
+import { PlanetDTO } from '../DTOS/Planet';
 
 export abstract class Validator {
   abstract validationData(data): boolean;
 }
 
 export class PlanetDataValidator extends Validator {
-  validationData(data: Planet): boolean {
+  validationData(data: PlanetDTO): boolean {
     if (!data.name) return false;
     if (!data.terrain) return false;
     if (!data.population) return false;
