c360cc0ca4eff344d6d67c37fd724f4bf3f051aa
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-19 17:37:49 -0300

    Adding the create method in the Characters Repository

--- src/repositories/CharRepository.ts
+++ src/repositories/CharRepository.ts
@@ -20,11 +20,35 @@ export class CharRepository implements Repository<CharactersDTO> {
 
     return Promise.all(chars);
   }
-  findOne(name: string): Promise<CharactersDTO> {
-    throw new Error('Method not implemented.');
+  async findOne(charName: string): Promise<CharactersDTO> {
+    const charFromDB = await prisma.character.findUnique({
+      where: { name: charName },
+    });
+
+    const originPlanetName = await prisma.planet.findUnique({
+      where: { id: charFromDB.originPlanetId },
+      select: { name: true },
+    });
+
+    const char = new CharactersDTO(charFromDB);
+
+    char.planetName = originPlanetName.name;
+
+    return char;
   }
-  create(data: CharactersDTO): Promise<CharactersDTO> {
-    throw new Error('Method not implemented.');
+  async create(data: CharactersDTO): Promise<CharactersDTO> {
+    const originPlanetId = await prisma.planet.findUnique({
+      where: { name: data.originPlanetName },
+      select: { id: true },
+    });
+
+    data.planetId = originPlanetId.id;
+
+    const newChar = await prisma.character.create({
+      data: data.coreInformation,
+    });
+
+    return new CharactersDTO(newChar);
   }
   update(data: CharactersDTO, name: string): Promise<CharactersDTO> {
     throw new Error('Method not implemented.');
