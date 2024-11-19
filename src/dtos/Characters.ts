b33a960db9326da7615dc17ba83c790310574558
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-19 17:18:26 -0300

    Creating the Character DTO

---
+++ src/dtos/Characters.ts
@@ -0,0 +1,52 @@
+export class CharactersDTO {
+  private _id: number;
+  private _name: string;
+  private _affiliation: string;
+  private _race: string;
+  private _originPlanetName: string;
+  private _originPlanetId: string;
+
+  constructor(data: {
+    id?: number;
+    name: string;
+    affiliation: string;
+    race: string;
+    originPlanetName?: string;
+    originPlanetId?: string;
+  }) {
+    this._id = data.id;
+    this._name = data.name;
+    this._affiliation = data.affiliation;
+    this._race = data.race;
+    this._originPlanetName = data.originPlanetName;
+    this._originPlanetId = data.originPlanetId;
+  }
+
+  get fullCharacter() {
+    return {
+      id: this._id,
+      name: this._name,
+      affiliation: this._affiliation,
+      race: this._race,
+      originPlanet: this._originPlanetName,
+    };
+  }
+
+  get coreInformation() {
+    return {
+      id: this._id,
+      name: this._name,
+      affiliation: this._affiliation,
+      race: this._race,
+      originPlanet: this._originPlanetName,
+    };
+  }
+
+  set planetId(id: string) {
+    this._originPlanetId = id;
+  }
+
+  set planetName(name: string) {
+    this._originPlanetName = name;
+  }
+}
